03586afd482b5fae88422f18b396715760fe6230
Joanaescar <joanaa-esteves@hotmail.com>
2022-12-09 23:18:54 +0000

    Improved forgot-password component

--- src/app/modals/forgot-password/forgot-password.component.ts
+++ src/app/modals/forgot-password/forgot-password.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css']
 })
-export class ForgotPasswordComponent {
+export class ForgotPasswordComponent implements OnInit {
+  closeIcon = faXmark;
 
+  constructor(private activeModal: NgbActiveModal, private modalService: NgbModal) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  dismissModal() {
+    this.activeModal.dismiss()
+  }
+
+  backModal() {
+    this.activeModal.close();
+
+    const modalRef = this.modalService.open(LoginComponent);
+    modalRef.componentInstance.name = 'World';
+  }
 }
